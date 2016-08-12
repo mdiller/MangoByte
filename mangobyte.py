80eaca9e0aa4317b5cd9790f3b5340a09c266412
dillerm <dillerm@oregonstate.edu>
2016-08-11 19:59:59 -0700

    added first working mangobot

---
+++ mangobyte.py
@@ -0,0 +1,61 @@
+import discord
+import asyncio
+import string
+from discord.ext import commands
+
+# http://people.oregonstate.edu/~dillerm/ResponsePlayer/index.php
+
+# /1/17/Treant_move_20
+# /3/3b/Troll_lose_03
+# /1/10/Timb_rare_01
+# /f/f1/Slark_rare_02
+# /e/e0/Drag_inthebag_01
+# /b/b7/Bristle_inthebag_01
+# /9/9d/Undying_gummy_vit_01
+# /b/b5/Undying_gummy_vit_03
+
+discord.opus.load_opus('libopus-0.x64.dll')
+
+description = '''a bot that does stuff n things.'''
+
+bot = commands.Bot(command_prefix='?', description=description)
+
+voice = None
+    
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command(pass_context=True)
+async def ping(ctx, count : int):
+    """Pings a number of times."""
+    ping_string = ""
+    for i in range(0, count):
+        ping_string += "Ping "
+    await bot.send_message(ctx.message.channel, ping_string, tts=True)
+
+@bot.command(pass_context=True)
+async def play(ctx, dota_response):
+    """Plays a dota response."""
+    global voice
+
+    if voice is None:
+        voice_channel = ctx.message.channel
+        for c in list(ctx.message.channel.server.channels):
+            if(c.name == 'General'):
+                voice_channel = c
+        voice = await bot.join_voice_channel(voice_channel)
+
+    try:
+        player = voice.create_ffmpeg_player('http://hydra-media.cursecdn.com/dota2.gamepedia.com' + dota_response + '.mp3')
+        player.start()
+    except Exception as e:
+        await bot.send_message(ctx.message.channel, "invalid input")
+
+f = open('token.txt', 'r')
+
+bot.run(f.read())
