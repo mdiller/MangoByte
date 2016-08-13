67520490791e0332ca8f49f82d9b8770ba5b1b06
dillerm <dillerm@oregonstate.edu>
2016-08-13 00:24:42 -0400

    added hello command and resource directory fix #8

--- mangobyte.py
+++ mangobyte.py
@@ -2,8 +2,7 @@ import discord
 import asyncio
 import string
 from discord.ext import commands
-
-# http://people.oregonstate.edu/~dillerm/ResponsePlayer/index.php
+from ctypes.util import find_library
 
 # /1/17/Treant_move_20
 # /3/3b/Troll_lose_03
@@ -14,7 +13,7 @@ from discord.ext import commands
 # /9/9d/Undying_gummy_vit_01
 # /b/b5/Undying_gummy_vit_03
 
-discord.opus.load_opus('libopus-0.x64.dll')
+discord.opus.load_opus(find_library('opus'))
 
 description = '''a bot that does stuff n things.'''
 
@@ -52,9 +51,32 @@ async def play(ctx, dota_response):
 
     try:
         player = voice.create_ffmpeg_player('http://hydra-media.cursecdn.com/dota2.gamepedia.com' + dota_response + '.mp3')
+        # dota mp3s are very loud, so make quieter
+        player.volume = 0.30
         player.start()
     except Exception as e:
+        print(str(e))
         await bot.send_message(ctx.message.channel, "invalid input")
+
+@bot.command(pass_context=True)
+async def hello(ctx):
+    """Says hello."""
+    global voice
+
+    if voice is None:
+        voice_channel = ctx.message.channel
+        for c in list(ctx.message.channel.server.channels):
+            if(c.name == 'General'):
+                voice_channel = c
+        voice = await bot.join_voice_channel(voice_channel)
+
+    try:
+        player = voice.create_ffmpeg_player('resource/hello.mp3')
+        player.start()
+    except Exception as e:
+        print(str(e))
+        await bot.send_message(ctx.message.channel, "error occured")
+    
 
 f = open('token.txt', 'r')
 
