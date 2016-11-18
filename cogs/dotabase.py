b3a0fcffd582217eaa9dc974a40979c15bcf9512
dillerm <dillerm@oregonstate.edu>
2016-11-18 00:13:41 -0500

    made dota command more intelligent, and can now search by hero

--- cogs/dotabase.py
+++ cogs/dotabase.py
@@ -17,6 +17,15 @@ class Dotabase:
 	"""
 	def __init__(self, bot):
 		self.bot = bot
+		self.hero_aliases = {}
+		self.build_hero_aliases()
+
+	def build_hero_aliases(self):
+		for hero in session.query(Hero):
+			self.hero_aliases[re.sub(r'[^a-z^A-Z]', r'', hero.name).lower()] = hero.id
+			self.hero_aliases[re.sub(r'[^a-z^A-Z]', r'', hero.localized_name).lower()] = hero.id
+			if " " in hero.localized_name:
+				self.hero_aliases["".join(e[0] for e in hero.localized_name.lower().split())] = hero.id
 
 	async def play_response(self, response):
 		response_file = "http://dotabase.me/dota-vpk" + response.mp3
@@ -28,7 +37,7 @@ class Dotabase:
 		await self.play_response(query.order_by(func.random()).first())
 
 	@commands.command(pass_context=True)
-	async def dota(self, ctx, *, query : str=None):
+	async def dota(self, ctx, *, keyphrase : str=None):
 		"""Plays a dota response
 
 		The format for input is the name of the sound.
@@ -47,31 +56,76 @@ class Dotabase:
 
 		If there is no response matching the input string, searches for any response that has the input string as part of its text 
 
+		To specify a specific hero to search for responses for, use ';' before the hero's name like this:
+		?dota ;rubick
+
 		To search for a response, try using the web tool at:
 		http://dotabase.me/responses/
 		ProTip: If you click the discord button next to the response, it will copy to your clipboard in the format needed to play using the bot."""
-		if(query == None):
-			await self.play_response_query(session.query(Response))
-			return
 
-		response = session.query(Response).filter(Response.name == query).first()
-		if not response is None:
-			await self.play_response(response)
-			return
+		if keyphrase is None:
+			words = []
+		else:
+			words = keyphrase.split(" ")
 
-		simple_input = " " + re.sub(r'[^a-z^0-9^A-Z^\s]', r'', query).lower() + " "
+		hero = None
+		for word in words:
+			if word.startswith(";"):
+				if not hero is None:
+					await self.bot.say("Thats too many heroes")
+					return
+				# Get hero id
+				hero = self.hero_aliases.get(word[1:])
+				# Remove word and surrounding whitespace from keyphrase
+				keyphrase = keyphrase.replace(word, "").strip()
 
-		query = session.query(Response).filter(Response.text_simple == simple_input)
-		if query.count() > 0:
+				if hero is None:
+					await self.bot.say("'" + word[1:] + "' isnt a hero")
+					return
+
+		query = await self.smart_dota_query(keyphrase, hero)
+
+		# If we didnt find any responses no hero was specified, check for hidden heroes
+		if query is None and hero is None:
+			for word in words:
+				hero = self.hero_aliases.get(word)
+				if not hero is None:
+					# Remove word and surrounding whitespace from keyphrase
+					keyphrase = keyphrase.replace(word, "").strip()
+					break
+
+			if not hero is None:
+				query = await self.smart_dota_query(keyphrase, hero)
+
+		if query is None:
+			await self.bot.say("No responses found! 😱");
+		else:
 			await self.play_response_query(query)
-			return
 
-		query = session.query(Response).filter(Response.text_simple.like("%" + simple_input + "%"))
+
+	async def smart_dota_query(self, keyphrase, hero=None):
+		basequery = session.query(Response)
+		if not hero is None:
+			basequery = basequery.filter(Response.hero_id == hero)
+
+		if keyphrase == None or keyphrase == "":
+			return basequery
+
+		query = basequery.filter(Response.name == keyphrase)
 		if query.count() > 0:
-			await self.play_response_query(query)
-			return
+			return query
 
-		await self.bot.say("Not a valid dota response, and no responses found with matching text 😱");
+		simple_input = " " + re.sub(r'[^a-z^0-9^A-Z^\s]', r'', keyphrase).lower() + " "
+
+		query = basequery.filter(Response.text_simple == simple_input)
+		if query.count() > 0:
+			return query
+
+		query = basequery.filter(Response.text_simple.like("%" + simple_input + "%"))
+		if query.count() > 0:
+			return query
+
+		return None
 
 	@commands.command(pass_context=True, aliases=["hi"])
 	async def hello(self, ctx):
