396cda859894c6dbfdb751be40c08a7f52eb68b2
dillerm <dillerm@oregonstate.edu>
2017-03-09 22:57:11 -0500

    updated mangobyte to be playing DOTA 3

--- mangobyte.py
+++ mangobyte.py
@@ -27,6 +27,7 @@ deprecated_commands = {}
 async def on_ready():
 	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
 	print('Connecting to voice channels if specified in botdata.json ...')
+	await bot.change_presence(game=discord.Game(name="DOTA 3 [?help]", url="http://github.com/mdiller/MangoByte"))
 	await bot.get_cog("DotaStats").init_dicts()
 	cog = bot.get_cog("Audio")
 
