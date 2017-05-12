3f7cd4684dc53a3b550a3f8b3dcd5fe248941aa4
dillerm <dillerm@oregonstate.edu>
2017-05-12 16:11:59 -0400

    removed resourcedir from settings.json

--- cogs/utils/settings.py
+++ cogs/utils/settings.py
@@ -11,7 +11,7 @@ from collections import OrderedDict
 class Settings:
 	def __init__(self):
 		self.path = "settings.json"
-		self.defaults = OrderedDict([  ("token", ""), ("resourcedir", "resource/"), ("ttslang", "en-au") ])
+		self.defaults = OrderedDict([  ("token", ""), ("ttslang", "en-au") ])
 		if not os.path.exists(self.path):
 			self.json_data = self.defaults
 			self.save_settings()
@@ -34,7 +34,7 @@ class Settings:
 
 	@property
 	def resourcedir(self):
-		return self.json_data["resourcedir"]
+		return "resource/" # self.json_data["resourcedir"]
 
 	@property
 	def ttslang(self):
