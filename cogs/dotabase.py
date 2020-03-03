c170d9b7124d5af3ac7b99fb8fdda045b228c5a7
Malcolm Diller <malcolm@dillerm.io>
2020-03-02 23:43:48 -0800

    adjusted hero count limits, and added footer for hero stats table

--- cogs/dotabase.py
+++ cogs/dotabase.py
@@ -1219,13 +1219,14 @@ class Dotabase(MangoCog):
 			raise UserError(f"Please select a stat to sort by. For a list of stats, see `{self.cmdpfx()}leveledstats`")
 		if table_args.hero_level < 1 or table_args.hero_level > 30:
 			raise UserError("Please select a hero level between 1 and 30")
-		if table_args.hero_count < 2 or table_args.hero_count > 140:
-			raise UserError("Please select a hero count between 2 and 140")
+		if table_args.hero_count < 2 or table_args.hero_count > 40:
+			raise UserError("Please select a hero count between 2 and 40")
 
 		embed = discord.Embed()
 
 		image = discord.File(await drawdota.draw_herostatstable(table_args, self.hero_stat_categories, self.leveled_hero_stats), "herotable.png")
 		embed.set_image(url=f"attachment://{image.filename}")
+		embed.set_footer(text="The stats shown above to not account for talents, passives, or items")
 
 		await ctx.send(embed=embed, file=image)
 	
